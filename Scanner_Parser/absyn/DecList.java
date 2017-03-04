2e63b5e5b4b96f16055c438ba7f23204bd882a00
bsaho <grissletoe@gmail.com>
2017-03-03 22:55:12 -0500

    filled in by Bash

---
+++ Scanner_Parser/absyn/DecList.java
@@ -0,0 +1,12 @@
+package absyn;
+
+public class DecList extends DecList  {
+  public Dec head;
+  public Dec tail;
+
+  public DecList(Dec head, DecList tail)  {
+    this.pos;
+    this.head = head;
+    this.tail = tail;
+  }
+}
---
+++ Scanner_Parser/absyn/VarDecList.java
@@ -0,0 +1,12 @@
+package absyn;
+
+public class VarDecList extends DecList  {
+  public VarDec head;
+  public VarDecList tail;
+
+  public VarDecList(VarDec head, VarDecList tail)  {
+    this.pos;
+    this.head = head;
+    this.tail = tail;
+  }
+}
