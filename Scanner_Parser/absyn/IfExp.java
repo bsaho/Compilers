91b9dbc37ee66b22e177a212f458c5ce3e3df8ad
sdeline <sdeline@mail.uoguelph.ca>
2017-03-05 08:50:06 -0500

    Fixed last error, added 2nd constructor for IfExp to deal with If-Then

--- Scanner_Parser/absyn/IfExp.java
+++ Scanner_Parser/absyn/IfExp.java
@@ -4,10 +4,17 @@ public class IfExp extends Exp {
   public Exp test;
   public Exp thenp;
   public Exp elsep;
+
   public IfExp( int pos, Exp test, Exp thenp, Exp elsep ) {
     this.pos = pos;
     this.test = test;
     this.thenp = thenp;
     this.elsep = elsep;
+  }
+  
+  public IfExp( int pos, Exp test, Exp thenp) {
+    this.pos = pos;
+    this.test = test;
+    this.thenp = thenp;
   }
 }
