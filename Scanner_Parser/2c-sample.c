31da23652828513908387bd3c93ae415357b17b4
bsaho <grissletoe@gmail.com>
2017-03-05 16:58:28 -0500

    larger test file

--- Scanner_Parser/2c-sample.c
+++ Scanner_Parser/2c-sample.c
@@ -1,17 +1,45 @@
-/* A program to perform selection on a 10 element array*/
+(/* A program to perform selection on a 10 element array*/
 
 int x[10];
-
-int minloc (int a[], int low, int high)
-{ int i; int x; int k;
+int z;
+int minloc (int a[], int low, int high){ 
+	int i; int x; int k;
 k=low;
+4 > 5;
+
 x=a[low];
 i=low + 1;
-while (i<high)
- { if (a[i] < x)
-	{x=a[i];
-	k=i;}
-	i=i+1;}
+while (i<high){ 
+ 	if (a[i] < x){
+ 	x=a[i];
+	k=i;
+	}
+	i=i+1;
+	}
+	100<99;
+3 <= 9;
+89 >= i;x = 1;
+
+9 / 3;
+
+6 + 12;
+
+8 != 156;
+156 - 99;
+1000 < 9;
+i > j;
+addition + aa;
+numerator / divisor;
+8 != 156;
+156 - 99;
+ red (1);
+ red ();
+ (x+y);
+ (x=y+2)
 	return k;
+	return ;
 	
 }
+/*
+
+*/)
